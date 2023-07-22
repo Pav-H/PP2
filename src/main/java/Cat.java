e0beafe8114b37a01fbd37e2dcff93277f0d1860
Pav-H <p.khomutov@gmail.com>
2023-07-23 01:19:18 +0300

    ADD: Solution of task 2.1.2

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+		private String name;
+		private int age;
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+	}
